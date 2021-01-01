0479b58f28089f20cbd4f5710c1ee5ab35553861
Hardik Dua <hardikdua08@gmail.com>
2021-01-01 11:49:44 +0530

    adapter done

--- app/src/main/java/com/news/nation/ui/adapters/DifferCallbacks.kt
+++ app/src/main/java/com/news/nation/ui/adapters/DifferCallbacks.kt
@@ -1,4 +1,16 @@
 package com.news.nation.ui.adapters
 
-class DifferCallbacks {
+import androidx.recyclerview.widget.DiffUtil
+import com.news.nation.models.Article
+
+object DifferCallbacks {
+    val newsDifferCallback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
 }
--- app/src/main/java/com/news/nation/ui/adapters/NewsAdapter.kt
+++ app/src/main/java/com/news/nation/ui/adapters/NewsAdapter.kt
@@ -1,4 +1,43 @@
 package com.news.nation.ui.adapters
 
-class NewsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.RecyclerView
+import com.news.nation.R
+import com.news.nation.databinding.ItemArticleCardBinding
+import com.news.nation.models.Article
+import com.news.nation.ui.adapters.DifferCallbacks.newsDifferCallback
+import com.news.nation.utils.Handler
+
+class NewsAdapter(@LayoutRes private val res: Int = R.layout.item_article_card) :
+    RecyclerView.Adapter<NewsViewHolder>() {
+
+    val differ = AsyncListDiffer(this, newsDifferCallback)
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val itemArticleCardBinding = DataBindingUtil.inflate<ItemArticleCardBinding>(
+            layoutInflater, res, parent, false
+        )
+        itemArticleCardBinding.handler = Handler.instance
+        return NewsViewHolder(itemArticleCardBinding)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val currentArticle = differ.currentList[position]
+        holder.bind(currentArticle)
+    }
+}
+
+class NewsViewHolder(binding: ItemArticleCardBinding) : RecyclerView.ViewHolder(binding.root) {
+    private val itemBinding = binding
+
+    fun bind(article: Article) {
+        itemBinding.article = article
+    }
 }
